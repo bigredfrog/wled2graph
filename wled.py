d9bc9c2fe4c4d969690ec95baecaf27d3a6f901e
atod <anthony.tod@gmail.com>
2024-02-10 16:45:51 -0500

    add led count to key

--- wled.py
+++ wled.py
@@ -15,5 +15,7 @@ def get_name(ip):
     url = f"http://{ip}/json/info"
     response = requests.get(url)
     response.raise_for_status()
-#    _LOGGER.info(f"response is {json.dumps(response.json(), indent=4)}")
-    return response.json()["name"]
+    resp_json = response.json()
+#    _LOGGER.info(f"response is {json.dumps(resp_json, indent=4)}")
+    name = f"{resp_json['name']} : {resp_json['leds']['count']}"
+    return name
