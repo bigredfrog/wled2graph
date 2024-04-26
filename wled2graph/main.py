edba309befb31d2452a67bbc351240138d59f69d
atod <anthony.tod@gmail.com>
2024-04-25 23:03:39 -0400

    adding to pypi

--- wled2graph/main.py
+++ wled2graph/main.py
@@ -6,23 +6,25 @@
 # specifically for recovering FPS values
 # these will be updated into the graph server
 
-import bokeh
 import logging
-import utils
-import graphs
-from args import Args
+from wled2graph import utils
+from wled2graph import graphs
+from wled2graph.args import Args
 
 args = Args()
 
 _LOGGER = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+def main():
     utils.setup_logging(logging.INFO)
 
     args.parse_args()
     _LOGGER.info(f"args are {args.args}")
 
     graphs.run_bokeh_app(args)
+
+if __name__ == '__main__':
+    main()
 
 
 
