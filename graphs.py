cb87bb4fda43fe2a6c5c46222ec3ed15b18afd2b
bigredfrog <anthony.tod@gmail.com>
2024-04-25 21:02:47 -0400

    Add wifi params, add table to page, remove -p option (#4)
    * adding wifi graphs and working on in page text
    
    * adding wifi graphs and working on in page text
    
    * adding wifi graphs and working on in page text
    
    * adding wifi graphs and working on in page text

--- graphs.py
+++ graphs.py
@@ -16,7 +16,10 @@ from bokeh.server.server import Server
 from bokeh.application import Application
 from bokeh.application.handlers.function import FunctionHandler
 from bokeh.palettes import Category10
-from bokeh.models import WheelZoomTool, PanTool
+from bokeh.models import WheelZoomTool, PanTool, Div
+
+# WARNING template is over ridden by bokeh in many cases!
+from template import css_template, css_table
 
 data_source = {}
 data_lock = Lock()
@@ -24,13 +27,23 @@ palette = Category10[10]
 
 graph_height = 480
 
+div_head = Div(text=f'<h1 style="font-size: 30px;"><a href="https://github.com/bigredfrog/wled2graph" target="_blank" style="color: inherit;">wled2graph</h1>')
+div_lines = Div(text="<p>Please hold</p>")
+
+# TODO: Need a distinction between those we graph and those we only hover
+fields = ['fps', 'rssi', 'bssid']
+paths = [['leds', 'fps'], ['wifi', 'rssi'], ['wifi', 'bssid']]
+
+
 def get_names(args):
     with data_lock:
         for ip in args.ip_list:
             _LOGGER.info(f"getting name for {ip}")
-            name = wled.get_name(args, ip)
-            _LOGGER.info(f"name : LED count = {name}")
-            data_source[str(ip)] = {"name":name, 'x': [], 'y': [], "p": []}
+            name, count = wled.get_name(args, ip)
+            _LOGGER.info(f"name : LED count = {name} : {count}")
+            data_source[str(ip)] = {'name':name, 'count':count, 'x': [], "p": []}
+            for field in fields:
+                data_source[str(ip)][field] = []
 
 
 def data_capture(args, start_time):
@@ -41,11 +54,13 @@ def data_capture(args, start_time):
 
         with data_lock:
             for ip in args.ip_list:
-                # make a call to the WLED JSON api and get the value of the param
                 if data_source[str(ip)]['name'] is not None:
-                    new_value = wled.get_param(args, ip, args.params[0])
+                    values = wled.get_param(args, ip, paths)
                     data_source[str(ip)]['x'].append(elapsed_time)
-                    data_source[str(ip)]['y'].append(new_value)
+                    for idx, value in enumerate(values):
+                        if value is None:
+                            continue
+                        data_source[str(ip)][fields[idx]].append(value)
                     # get ping for ip
                     new_ping = wled.get_ping(args, ip)
                     data_source[str(ip)]['p'].append(new_ping)
@@ -57,19 +72,24 @@ def open_browser():
 def make_document(doc, args):
 
     source_dict = {}
-    param = args.params[0]
-    plot_params = figure(title=f"Real-time update for {param}",
-                  x_axis_label="time (s)", y_axis_label=f"{param}", width=1500,
+    # TODO: create graphs data driven from fields
+    plot_params = figure(title=f"Real-time update for fps",
+                  x_axis_label="time (s)", y_axis_label=f"FPS", width=1500,
                   height=graph_height)
 
     plot_ping = figure(title="Real-time update for ping",
                   x_axis_label="time (s)", y_axis_label="ping (ms)", width=1500,
                   height=graph_height, x_range=plot_params.x_range)
 
+    plot_rssi = figure(title="Real-time update for rssi",
+                  x_axis_label="time (s)", y_axis_label="rssi", width=1500,
+                  height=graph_height, x_range=plot_params.x_range)
+
     for index, ip in enumerate(data_source.keys()):
-        source_dict[ip] = ColumnDataSource(data=dict(x=[], y=[], p=[]))
+        # TODO: make this data driven for DICT
+        source_dict[ip] = ColumnDataSource(data=dict(x=[], rssi=[], fps=[], bssid=[], p=[]))
         full_name = f"{ip}: {data_source[ip]['name']}"
-        plot_params.line("x", "y",
+        plot_params.line("x", "fps",
                   source=source_dict[ip],
                   name=full_name,
                   color=palette[index % len(palette)],
@@ -83,41 +103,61 @@ def make_document(doc, args):
                     legend_label=full_name,
                     line_width=4)
 
+        plot_rssi.line("x", "rssi",
+                    source=source_dict[ip],
+                    name=full_name,
+                    color=palette[index % len(palette)],
+                    legend_label=full_name,
+                    line_width=4)
+
     plot_params.legend.location = "top_left"
     plot_params.legend.click_policy = "hide"
 
     plot_ping.legend.location = "top_left"
     plot_ping.legend.click_policy = "hide"
 
+    plot_rssi.legend.location = "top_left"
+    plot_rssi.legend.click_policy = "hide"
+
     xwheel_zoom = WheelZoomTool(dimensions="width")
     plot_params.add_tools(xwheel_zoom)
     plot_params.toolbar.active_scroll = xwheel_zoom
     plot_ping.add_tools(xwheel_zoom)
     plot_ping.toolbar.active_scroll = xwheel_zoom
+    plot_rssi.add_tools(xwheel_zoom)
+    plot_rssi.toolbar.active_scroll = xwheel_zoom
 
     x_pan = PanTool(dimensions="width")
     plot_params.add_tools(x_pan)
     plot_params.toolbar.active_drag = x_pan
     plot_ping.add_tools(x_pan)
     plot_ping.toolbar.active_drag = x_pan
+    plot_rssi.add_tools(x_pan)
+    plot_rssi.toolbar.active_drag = x_pan
 
     doc.theme = "dark_minimal"
 
     custom_tooltip = """
         <div style="background: black; margin:-10px; padding: 10px; border-radius: 10px; border: 1px solid white; font-weight: 600; font-size: 14px;">
-            <div style="color: white;">key: $name</div>
-            <div style="color: white;">fps: @y</div>
-            <div style="color: white;">png: @p ms</div>
+            <div style="color: white;">key  : $name</div>
+            <div style="color: white;">fps  : @fps</div>
+            <div style="color: white;">rssi : @rssi</div>
+            <div style="color: white;">ping : @p</div>
+            <div style="color: white;">bssid: @bssid</div>
         </div>
     """
 
     hover = HoverTool(tooltips=custom_tooltip)
     plot_params.add_tools(hover)
     plot_ping.add_tools(hover)
+    plot_rssi.add_tools(hover)
 
     def update():
         with data_lock:
-            for ip in data_source.keys():
+            new_table_html = f"<table class='my-table'>"
+            new_table_html += f"<tr><th>IP</th><th>Name</th><th>  Leds  </th><th>   FPS   </th><th>BSSID</th><th>  RSSI  </th><th>Ping(ms)</th></tr>"
+
+            for idx, ip in enumerate(data_source.keys()):
                 x_values = source_dict[ip].data['x']
                 last_x = x_values[-1] if x_values else -float('inf')
 
@@ -133,13 +173,28 @@ def make_document(doc, args):
                     # It means we have new data points to stream
                     new_data = {
                         'x': data_source[ip]['x'][start_index:],
-                        'y': data_source[ip]['y'][start_index:],
+                        'fps': data_source[ip]['fps'][start_index:],
+                        'rssi': data_source[ip]['rssi'][start_index:],
+                        'bssid': data_source[ip]['bssid'][start_index:],
                         'p': data_source[ip]['p'][start_index:]
                     }
                     source_dict[ip].stream(new_data, rollover=args.args.rollover)
+                cl = Category10[10][idx % 10]
+                td_style = f"<td style='color: {cl};'>"
+                new_table_html += f"<tr>{td_style}<a href='http://{ip}' target='_blank' style='color: inherit;'>{ip}</td>"+\
+                                  f"{td_style}{data_source[ip]['name']}</td>"+\
+                                  f"<td>{data_source[ip]['count']}</td>"+\
+                                  f"<td>{data_source[ip]['fps'][-1]}</td>"+\
+                                  f"<td>{data_source[ip]['bssid'][-1]}</td>"+\
+                                  f"<td>{data_source[ip]['rssi'][-1]}</td>"+\
+                                  f"<td>{data_source[ip]['p'][-1]:.0f}</td></tr>"
+
+            new_table_html += '</table>'
+            div_lines.text = f"{css_table}{new_table_html}"
 
     doc.add_periodic_callback(update, args.args.period * 1000)
-    doc.add_root(column(plot_params, plot_ping))
+    doc.template = css_template
+    doc.add_root(column(div_head, div_lines, plot_params, plot_ping, plot_rssi))
 
 
 def run_bokeh_app(args):
---
+++ template.py
@@ -0,0 +1,46 @@
+css_template = """
+{% block head %}
+<style>
+    body {
+        background-color: #333;
+        color: #fff;
+        font-family: 'Arial', sans-serif;
+    }
+    h1 {
+        font-size: 24px;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #fff;
+        padding: 8px;
+        color: #fff;
+        font-size: 16px;
+    }
+    th {
+        background-color: #555;
+    }
+</style>
+{{ super() }}
+{% endblock %}
+"""
+
+css_table = """
+<style>
+    .my-table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    .my-table th, .my-table td {
+        border: 1px solid #fff;
+        padding: 12px;
+        color: #fff;
+        font-size: 20px;
+    }
+    .my-table th {
+        background-color: #555;
+    }
+</style>
+"""
